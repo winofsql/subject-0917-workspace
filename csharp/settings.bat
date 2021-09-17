bd86be55fb7ee1d34e15ae9e249da3b004175d83
sworc <lightbox@nifty.com>
2021-09-17 15:05:40 +0900

    Add files via upload

--- csharp/settings.bat
+++ csharp/settings.bat
@@ -3,5 +3,8 @@ echo {> .vscode\settings.json
 echo     "code-runner.showRunIconInEditorTitleMenu": true,>> .vscode\settings.json
 echo     "code-runner.runInTerminal": true,>> .vscode\settings.json
 echo     "code-runner.fileDirectoryAsCwd": true,>> .vscode\settings.json
+echo     "code-runner.executorMap": {>> .vscode\settings.json
+echo         "csharp": "dotnet run">> .vscode\settings.json
+echo     },>> .vscode\settings.json
 echo     "terminal.integrated.fontSize": 16>> .vscode\settings.json
 echo }>> .vscode\settings.json
